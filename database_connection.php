83364fafb692496eb35ec359984f113bf399935a
raj00-verma <166989282+raj00-verma@users.noreply.github.com>
2024-04-14 22:31:06 +0530

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -2,7 +2,7 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=localhost; dbname=lms", "root", "");
+$connect = new PDO("mysql:host=3.92.186.136; dbname=lms", "root", "");
 
 session_start();
 
